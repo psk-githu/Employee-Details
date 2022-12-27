9e7eb0ef65075c8931b6b5ca49915110930d0f05
psk-githu <prashantskapse98@gmail.com>
2022-12-27 10:29:58 +0530

    added create read delete operation

--- src/main/java/com/EmployeeDetails/controller/EmployeeController.java
+++ src/main/java/com/EmployeeDetails/controller/EmployeeController.java
@@ -1,5 +1,44 @@
 package com.EmployeeDetails.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.EmployeeDetails.domain.EmployeeDetail;
+import com.EmployeeDetails.exception.CustomException;
+import com.EmployeeDetails.service.EmployeeService;
+
+@RestController
 public class EmployeeController {
 
+	@Autowired
+	private EmployeeService service;
+	
+	@GetMapping("/list")
+	public List<EmployeeDetail> allEmployee(){
+		return service.listAllEmployee();
+	}
+	@GetMapping("/employee/{id}")
+	public Optional<EmployeeDetail> employeeById(@PathVariable int id){
+		return service.getEmployeeById(id);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteEmployee(@PathVariable Integer id) throws CustomException {
+		service.removeEmployeeById(id);
+		return "Deleted Successfully";
+	}
+	
+	@PostMapping("/add")
+	public String saveEmployee(EmployeeDetail employee) {
+		service.addEmployee(employee);
+		return "Added Successfully";
+	}	
+	
 }
--- src/main/java/com/EmployeeDetails/domain/EmployeeDetail.java
+++ src/main/java/com/EmployeeDetails/domain/EmployeeDetail.java
@@ -1,5 +1,73 @@
 package com.EmployeeDetails.domain;
 
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity(name="EMPLOYEE_DETAIL")
 public class EmployeeDetail {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer employeeID;
+	private String employeeName;
+	private String emailId;
+	private Long mobileNo;
+	private String location;
+	private Date joiningDate;
+	
+	public EmployeeDetail(Integer employeeID, String employeeName, String emailId, Long mobileNo, String location,
+			Date joiningDate) {
+		this.employeeID = employeeID;
+		this.employeeName = employeeName;
+		this.emailId = emailId;
+		this.mobileNo = mobileNo;
+		this.location = location;
+		this.joiningDate = joiningDate;
+	}
+	public EmployeeDetail() {}
+	public Integer getEmployeeID() {
+		return employeeID;
+	}
+	public void setEmployeeID(Integer employeeID) {
+		this.employeeID = employeeID;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public Long getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(Long mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public Date getJoiningDate() {
+		return joiningDate;
+	}
+	public void setJoiningDate(Date joiningDate) {
+		this.joiningDate = joiningDate;
+	}
+	
+	
+	
 
 }
